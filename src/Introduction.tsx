dbbb3fdf1b51dfd632c2e3c52b7497b0f0981511
blck-snwmn <whitesnowmancreator@gmail.com>
2024-05-06 15:57:45 +0900

    Change layout

--- src/Introduction.tsx
+++ src/Introduction.tsx
@@ -20,13 +20,15 @@ export const IntroductionCard: React.FC<IntroductionCard> = ({
 	bgColor,
 }) => {
 	return (
-		<div className="flex p-3 rounded-xl" style={{ backgroundColor: bgColor }}>
-			<div className="flex flex-col items-center w-64 mr-5">
+		<div className="flex rounded-xl h-2/3 w-5/6 p-5" style={{ backgroundColor: bgColor }}>
+			<div className="flex flex-col items-center justify-center w-2/6">
 				<Img src={icon} alt="Profile Icon" className="rounded-full" />
 				<h2 className="text-2xl">{name}</h2>
 			</div>
-			<div className="text-base/loose w-96 bg-slate-50 p-2 rounded-xl">
-				{description}
+			<div className="flex justify-center w-4/6 py-3 pl-5">
+				<div className="text-base/loose bg-slate-50 p-2 rounded-xl ">
+					{description}
+				</div>
 			</div>
 		</div>
 	);
