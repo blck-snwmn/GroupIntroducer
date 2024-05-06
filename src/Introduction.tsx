aba26bf29c422719c89a734853d800d3ea0ad778
blck-snwmn <whitesnowmancreator@gmail.com>
2024-05-06 20:00:17 +0900

    Adjust frame

--- src/Introduction.tsx
+++ src/Introduction.tsx
@@ -23,10 +23,10 @@ export const IntroductionCard: React.FC<IntroductionCard> = ({
 		<div className="flex rounded-xl h-2/3 w-5/6 p-5" style={{ backgroundColor: bgColor }}>
 			<div className="flex flex-col items-center justify-center w-2/6">
 				<Img src={icon} alt="Profile Icon" className="rounded-full" />
-				<h2 className="text-2xl">{name}</h2>
+				<h2 className="text-3xl mt-5">{name}</h2>
 			</div>
 			<div className="flex justify-center w-4/6 py-3 pl-5">
-				<div className="text-base/loose bg-slate-50 p-2 rounded-xl ">
+				<div className="text-xl/loose bg-slate-50 p-2 rounded-xl ">
 					{description}
 				</div>
 			</div>
