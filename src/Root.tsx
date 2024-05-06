1aaa00fff90418a9956dc2a3d0bba2795b190bf2
blck-snwmn <whitesnowmancreator@gmail.com>
2024-05-06 20:00:26 +0900

    Adjust frame

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,12 +3,17 @@ import { Introduction } from "./Composition";
 import "./style.css";
 
 export const RemotionRoot: React.FC = () => {
+	const ITEM_NUMBER = 5; // change your item number here
+	const FRAM_PER_ITEM = 120;
+	const FRAM_PER_TRANSITION = 30;
+	const DURATION = ITEM_NUMBER * FRAM_PER_ITEM - (ITEM_NUMBER + 1) * FRAM_PER_TRANSITION;
+
 	return (
 		<>
 			<Composition
 				id="Introductions"
 				component={Introduction}
-				durationInFrames={330}
+				durationInFrames={DURATION}
 				fps={30}
 				width={1280}
 				height={720}
